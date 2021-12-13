cf45c31f1590cc185ffc1643a385e633e4f32dd3
kevin martin <kmartin.dev@gmail.com>
2021-12-13 12:47:30 +0100

    Update gradle to 7 and kotlin to 1.6

--- app/src/main/java/com/km/civilian/cpr/database/AppDatabase.kt
+++ app/src/main/java/com/km/civilian/cpr/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.km.civilian.cpr.model.Message
 
-@Database(entities = [Message::class], version = 4)
+@Database(entities = [Message::class], version = 4, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun messageDao(): MessageDao
