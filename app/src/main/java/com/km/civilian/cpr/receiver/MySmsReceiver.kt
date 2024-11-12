b9b1780f033e32465e5b0aea88a7675679bc2fb9
Kevin Martin <dev.kmartin@gmail.com>
2024-11-12 13:51:09 +0100

    refactor: SmsReceiver to use CoroutineScope in favour of GlobalScope

--- app/src/main/java/com/km/civilian/cpr/receiver/MySmsReceiver.kt
+++ app/src/main/java/com/km/civilian/cpr/receiver/MySmsReceiver.kt
@@ -3,17 +3,15 @@ package com.km.civilian.cpr.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.Bundle
 import android.provider.Telephony
-import android.telephony.SmsMessage
 import com.km.civilian.cpr.model.Message
 import com.km.civilian.cpr.repository.IMessageRepository
 import com.km.civilian.cpr.util.Constants
 import com.km.civilian.cpr.util.NotificationBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -24,18 +22,28 @@ class MySmsReceiver : BroadcastReceiver() {
     @Inject
     lateinit var messageRepository: IMessageRepository
 
+    // Create a coroutine job to manage coroutines lifecycle in BroadcastReceiver
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
+
     /**
      * Listens for sms messages received.
      * Stores non empty messages in the database and send out a notification if the message type is known
      */
     override fun onReceive(context: Context, intent: Intent) {
+        // Check that the intent action is the expected one (e.g., SMS_RECEIVED action)
+        if (intent.action != "android.provider.Telephony.SMS_RECEIVED") {
+            // If the action doesn't match, ignore the intent to avoid unwanted behavior
+            return
+        }
+
         // Get the SMS message.
-        getTextFromHartstichtingSms(intent.extras, intent)?.let { textMessage ->
+        getTextFromHartstichtingSms(intent)?.let { textMessage ->
             val message = Message(0, textMessage, Date())
 
             // Insert the message in the app database if it contains text.
             if (message.text.isNotBlank()) {
-                GlobalScope.launch(Dispatchers.IO) {
+                scope.launch {
                     messageRepository.insert(message)
                 }
             }
@@ -51,7 +59,7 @@ class MySmsReceiver : BroadcastReceiver() {
      * @param extras Bundle containing the sms data.
      * @return String? text from the sms if it's from the hartstichting otherwise null.
      */
-    private fun getTextFromHartstichtingSms(extras: Bundle?, intent: Intent): String? {
+    private fun getTextFromHartstichtingSms(intent: Intent): String? {
 
         val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
 
@@ -68,21 +76,4 @@ class MySmsReceiver : BroadcastReceiver() {
         return if (phoneNumber?.equals(Constants.HARTSTICHTING_PHONE_NUMBER, ignoreCase = true) == true) messageSB.toString() else null
     }
 
-    /**
-     * Constructs a SmsMessage object from a pdu ByteArray
-     *
-     * @param pdu ByteArray of the pdu
-     * @param format The format the pdu is described in.
-     * @return SmsMessage created from [pdu].
-     */
-    private fun getSmsMessage(pdu: ByteArray?, format: String?): SmsMessage? {
-        return when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> SmsMessage.createFromPdu(
-                pdu,
-                format
-            )
-
-            else -> SmsMessage.createFromPdu(pdu)
-        }
-    }
 }
